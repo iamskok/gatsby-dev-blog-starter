bf132f32a9695afe809bf7149255374edecbfc68
Vladimir Skok <skok@vova.io>
2018-12-26 20:52:25 +0400

    feat: add home, 404, contact, about and blog pages

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const AboutPage = () => (
+	<Layout>
+		<SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
+		<h1>About page</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex saepe odio quidem culpa 
+		fugit nostrum sit dolorum velit fuga placeat quo, perferendis explicabo expedita dolores 
+		maiores eveniet. Fugit.</p>
+	</Layout>
+);
+
+export default AboutPage
