cf155c3c3b922aa74aeb3e2d3d85218bd49b876d
Vladimir Skok <iamskok@users.noreply.github.com>
2018-12-27 21:35:58 +0400

    feat(view): add Header component
    Add material-ui framework. Create Logo, Nav and Header components. Import Header in Layout component.

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,39 +6,42 @@ import Header from './header'
 import './layout.css'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+	<StaticQuery
+		query={graphql`
+			query SiteTitleQuery {
+				site {
+					siteMetadata {
+						title
+					}
+				}
+			}
+		`}
+		render={data => (
+			<>
+				<Header siteTitle={data.site.siteMetadata.title} />
+				<div
+					style={{
+						margin: `0 auto`,
+						maxWidth: 960,
+						padding: `0px 1.0875rem 1.45rem`,
+						paddingTop: 0,
+					}}
+				>
+					{children}
+					<footer>
+						© 2018, Built with&nbsp;
+						<a href="https://www.github.com/iamskok/gatsby-dev-blog-starter">
+							Gatsby Dev Blog Starter
+						</a>
+					</footer>
+				</div>
+			</>
+		)}
+	/>
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 }
 
-export default Layout
+export default Layout;
---
+++ src/components/logo.js
@@ -0,0 +1,15 @@
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+export default withStyles({
+	root: {
+		'&:hover': {
+			backgroundColor: 'rgba(0,0,0,0)',
+		}
+	},
+	label: {
+		'&:hover': {
+			color: '#fff'
+		}
+	}
+})(Button);
