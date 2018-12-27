cedeb8b96560e476ab675d3ee01ee05787d0f65b
Vladimir Skok <skok@vova.io>
2018-12-27 22:03:24 +0400

    feat: add post listing to index and blog pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,58 @@
 import React from 'react';
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
-	<Layout>
-		<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-		<h1>Home page</h1>
-		<p>Welcome to your new Gatsby dev blog.</p>
-		<p>Now go build something great.</p>
-		<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-			<Image />
-		</div>
-	</Layout>
-);
+const IndexPage = (props) => {
+	const posts = props.data.allMarkdownRemark.edges;
+
+	return (
+		<Layout>
+			<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+			<h1>Home page</h1>
+			<p>Welcome to your new Gatsby dev blog.</p>
+			<p>Now go build something great.</p>
+			
+			<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+				<Image />
+			</div>
+
+			<ul>
+				{posts.map(post => (
+				<li key={post.node.frontmatter.path}>
+					<Link to={post.node.frontmatter.path}>{post.node.frontmatter.title}</Link>
+				</li>
+				))}
+			</ul>
+		</Layout>
+	)
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+	query IndexQuery {
+		site {
+			siteMetadata {
+				title
+				author
+			}
+		}
+	allMarkdownRemark(
+		limit: 5
+		sort: { fields: [frontmatter___date], order: DESC }
+	) {
+		edges {
+			node {
+				id
+				frontmatter {
+					title
+					date
+					path
+				}
+			}
+		}
+	}
+}`
