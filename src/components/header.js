ab8189683fd8f8f1e08e324920a1032c8e49b6a8
Vladimir Skok <skok@vova.io>
2019-01-06 23:32:23 +0400

    feat(view): add Logo and LogoIcon components in Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,37 @@
-import { Link } from 'gatsby'
+import {Link} from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
-
+import {withStyles} from '@material-ui/core/styles';
 import Logo from './logo';
+import LogoIcon from './logoIcon';
 import Nav from './nav';
 
 function Header(props) {
-	const { classes } = props;
+	const {classes} = props;
 	return (
-	<div className={classes.root}>
-		<AppBar position="static">
-			<Nav>
-				<Logo
-					component={Link} 
-					disableRipple={true}
-					className="button"
-					to="/"
-				>
-					{props.siteTitle}
-				</Logo>
-				<div className="navContainer">
-					<Button component={Link} to="/blog">Blog</Button>
-					<Button component={Link} to="/about">About</Button>
-					<Button component={Link} to="/contact">Contact</Button>
-				</div>
-			</Nav>
-	  </AppBar>
-	</div>
-  );
-}
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Nav>
+					<Logo
+						component={Link}
+						disableRipple={true}
+						className="button"
+						to="/"
+					>
+						<LogoIcon />
+					</Logo>
+					<div className="navContainer">
+						<Button component={Link} to="/blog">Blog</Button>
+						<Button component={Link} to="/about">About</Button>
+						<Button component={Link} to="/contact">Contact</Button>
+					</div>
+				</Nav>
+			</AppBar>
+		</div>
+	);
+};
 
 Header.propTypes = {
 	classes: PropTypes.object.isRequired
