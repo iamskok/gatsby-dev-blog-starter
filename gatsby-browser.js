b16883b53b4468599cc8ee57eddfc87ed401c5d5
Vladimir Skok <skok@vova.io>
2019-01-06 23:20:31 +0400

    feat(view): wrap root component in MuiThemeProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react"
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from './src/components/theme';
+
+export const wrapRootElement = ({element}) => {
+	return (
+		<MuiThemeProvider theme={theme}>
+			{element}
+		</MuiThemeProvider>
+	);
+};
