855cc2ce9df61f4479b6d81f5b91d4a7c0abfe43
Vladimir Skok <skok@vova.io>
2019-01-06 23:52:58 +0400

    feat(view): update blog page
    Import Grid and Card components in blog page. Add childImageSharp qraphQL query (responsive images for 1x, 1.5x, 2x, 2.5x, 3x, 3.5x, 4x device pixel ratios and all required media queries).

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
+import {graphql} from 'gatsby';
+import Grid from '@material-ui/core/Grid';
 import Layout from '../components/layout';
+import Card from '../components/card';
 import SEO from '../components/seo';
 
 const BlogPage = (props) => {
@@ -11,20 +12,28 @@ const BlogPage = (props) => {
 		<Layout>
 			<SEO title="Blog" keywords={[`gatsby`, `application`, `react`]} />
 			<h1>Blog page</h1>
+
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex saepe odio quidem culpa 
 			fugit nostrum sit dolorum velit fuga placeat quo, perferendis explicabo expedita dolores 
 			maiores eveniet. Fugit.</p>
 
-			{posts.map(post => (
-				<li key={post.node.frontmatter.path}>
-					<Link to={post.node.frontmatter.path}>{post.node.frontmatter.title}</Link>
-				</li>
-			))}
+			<Grid container spacing={24}>
+				{posts.map((post, index) => (
+					<Grid item xs={12} md={6} key={index}>
+						<Card
+							title={post.node.frontmatter.title}
+							excerpt={post.node.excerpt}
+							link={post.node.frontmatter.path}
+							cover={post.node.frontmatter.cover.childImageSharp.fluid}
+						/>
+					</Grid>
+				))}
+			</Grid>
 		</Layout>
 	)
-}
+};
 
-export default BlogPage
+export default BlogPage;
 
 export const pageQuery = graphql`
 	query BlogQuery {
@@ -40,12 +49,49 @@ export const pageQuery = graphql`
 	) {
 		edges {
 			node {
+				id
+				excerpt(pruneLength: 280)
 				frontmatter {
 					title
 					date
 					path
+					cover {
+						publicURL
+						childImageSharp {
+							fluid(
+								maxWidth: 290
+								srcSetBreakpoints: [
+									290,
+									330,
+									380,
+									435,
+									495,
+									570,
+									610,
+									660,
+									725,
+									780,
+									825,
+									880,
+									950,
+									1100,
+									1160,
+									1225,
+									1320,
+									1470,
+									1560,
+									1715,
+									1830,
+									1960,
+									2135,
+									2440
+							]) {
+								...GatsbyImageSharpFluid_withWebp
+							}
+						}
+					}
 				}
 			}
 		}
 	}
-}`
+}`;
