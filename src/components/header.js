6fa3129a6f27c6057822c72b411a03e955576b0c
Vladimir Skok <skok@vova.io>
2019-01-09 19:24:54 +0400

    feat(view): update header component
    Change Header component styles. Install react-headroom plugin to hide header until it's needed.

--- src/components/header.js
+++ src/components/header.js
@@ -7,29 +7,39 @@ import {withStyles} from '@material-ui/core/styles';
 import Logo from './logo';
 import LogoIcon from './logoIcon';
 import Nav from './nav';
+import Headroom from 'react-headroom';
+import './headroom.css';
 
 function Header(props) {
 	const {classes} = props;
 	return (
-		<div className={classes.root}>
-			<AppBar position="static">
-				<Nav>
-					<Logo
-						component={Link}
-						disableRipple={true}
-						className="button"
-						to="/"
-					>
-						<LogoIcon />
-					</Logo>
-					<div className="navContainer">
-						<Button component={Link} to="/blog">Blog</Button>
-						<Button component={Link} to="/about">About</Button>
-						<Button component={Link} to="/contact">Contact</Button>
-					</div>
-				</Nav>
-			</AppBar>
-		</div>
+		<Headroom className="headroom--unpinned">
+			<div className={classes.root}>
+				<AppBar position="static">
+					<Nav>
+						<Logo
+							component={Link}
+							disableRipple={true}
+							className="button"
+							to="/"
+						>
+							<LogoIcon />
+						</Logo>
+						<div>
+							<Button component={Link} className={classes.button} to="/blog">
+								Blog
+							</Button>
+							<Button component={Link} className={classes.button} to="/about">
+								About
+							</Button>
+							<Button component={Link} className={classes.button} to="/contact">
+								Contact
+							</Button>
+						</div>
+					</Nav>
+				</AppBar>
+			</div>
+		</Headroom>
 	);
 };
 
@@ -40,5 +50,11 @@ Header.propTypes = {
 export default withStyles({
 	root: {
 		marginBottom: 20
+	},
+	button: {
+		color: '#fff',
+		'&:hover': {
+			backgroundColor: 'rgba(255, 255, 255, 0.05)'
+		}
 	}
 })(Header);
