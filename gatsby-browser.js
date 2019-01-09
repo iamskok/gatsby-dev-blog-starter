3464e75c52c47a4180b004937b2be665f36ce67a
Vladimir Skok <skok@vova.io>
2019-01-09 19:38:33 +0400

    style: edit code styling for gatsby-browser.js and Post template

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,5 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
 import React from "react"
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import {MuiThemeProvider} from '@material-ui/core/styles';
 import theme from './src/components/theme';
 
 export const wrapRootElement = ({element}) => {
--- src/templates/post.js
+++ src/templates/post.js
@@ -4,14 +4,16 @@ import Layout from '../components/layout';
 import Typography from '@material-ui/core/Typography';
 
 export default function Template({data}) {
-	const { markdownRemark } = data;
-	const { frontmatter, html } = markdownRemark;
+	const {markdownRemark} = data;
+	const {frontmatter, html} = markdownRemark;
 	return (
 		<Layout>
 			<div className="blog-post-container">
-				<Typography gutterBottom variant="h3" component="h1">{frontmatter.title}</Typography>
+				<Typography gutterBottom variant="h3" component="h1">
+					{frontmatter.title}
+				</Typography>
 				<Typography gutterBottom variant="h5">{frontmatter.date}</Typography>
-
+				
 				<div dangerouslySetInnerHTML={{ __html: html }} />
 			</div>
 		</Layout>
