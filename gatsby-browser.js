98daef98e1958facfe5b0be793bd75be22607f9b
Vladimir Skok <skok@vova.io>
2019-01-12 16:01:15 +0400

    feat(view): add dracula theme for syntax highlighting
    Install gatsby-syntax-highlight-dracula and update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {MuiThemeProvider} from '@material-ui/core/styles';
 import theme from './src/components/theme';
+import 'gatsby-syntax-highlight-dracula';
 
 export const wrapRootElement = ({element}) => {
 	return (
