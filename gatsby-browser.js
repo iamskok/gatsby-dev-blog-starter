72b8940277197e8f204491918af2c7a35c2d5d3d
Vladimir Skok <skok@vova.io>
2019-01-27 15:04:47 +0400

    feat(view): add gatsby-prismjs-dracula syntax highligting theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,7 @@
 import React from "react";
 import {MuiThemeProvider} from '@material-ui/core/styles';
 import theme from './src/components/theme';
-import 'gatsby-syntax-highlight-dracula';
-import './src/styles/autolink-headers.scss';
-import './src/styles/code-title.scss';
-import './src/styles/code-button.scss';
+import 'gatsby-prismjs-dracula';
 
 export const wrapRootElement = ({element}) => {
 	return (
