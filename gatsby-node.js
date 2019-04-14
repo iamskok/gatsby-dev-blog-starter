04755bcb6275a51bf981086e381c97efe2a81a11
Vladimir Skok <skok@vova.io>
2019-04-14 02:02:06 -0700

    fix(build): fix netlify time exceed error

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,3 +41,9 @@ exports.createPages = ({actions, graphql}) => {
 		});
 	});
 };
+
+const ChildProcess = require('child_process');
+
+exports.onPostBuild = () => {
+	ChildProcess.execSync(`ps aux | grep jest | grep -v grep | awk '{print $2}' | xargs kill`);
+};
