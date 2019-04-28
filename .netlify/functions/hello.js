7e831536e48c1020032f7154a0effe7b124b6c74
Vladimir Skok <skok@vova.io>
2019-04-28 10:54:17 -0700

    feat: add netlify test function

---
+++ .netlify/functions/hello.js
@@ -0,0 +1,7 @@
+exports.handler = (event, context, callback) => {
+  console.log("queryStringParameters", event.queryStringParameters)
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ msg: "Hello, World!" })
+  })
+}
