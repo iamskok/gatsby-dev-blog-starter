8daa2967ae4c9b2949655eaa60f5ebe5d6a2ed5e
Vladimir Skok <skok@vova.io>
2019-07-21 15:06:08 -0700

    fix(build): edit access-token function

--- src/functions/access-token.js
+++ src/functions/access-token.js
@@ -65,6 +65,10 @@ if (process.env.CONTEXT === 'production') {
   console.error('process.env.CONTEXT = ', process.env.CONTEXT);
 }
 
+host = 'https://gatsby-dev-blog-starter.netlify.com'
+clientId = '48c2d50f608bd71d4aac'
+password = 'MX#MC&fhati1m0jAEtKCV%QeXcNTX%3khlCsjf^hiuPZVaP2EMCqmdYWC6f^SSnJx3N8llqbR1$UBkh1^YG7MrQb41aR$%CA*JTm'
+
 exports.handler = (event, context, callback) => {
   axios.get(`${host}/.netlify/functions/vault?password=${password}`)
     .then(res => {
