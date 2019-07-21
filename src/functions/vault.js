d71233f185ccb06f9c905d037bea206d0a7fb5f5
Vladimir Skok <skok@vova.io>
2019-07-21 13:46:10 -0700

    fix(build): edit vault netlify function logic

--- src/functions/vault.js
+++ src/functions/vault.js
@@ -13,12 +13,12 @@ exports.handler = (event, context, callback) => {
   if (event.httpMethod === 'POST') {
     const body = JSON.parse(event.body)
 
-    if (body.password !== password) {
-      return callback(null, {
-        statusCode: 403,
-        body: 'Access denied'
-      })
-    }
+    // if (body.password !== password) {
+    //   return callback(null, {
+    //     statusCode: 403,
+    //     body: 'Access denied'
+    //   })
+    // }
 
     global.state = body.state
 
@@ -27,12 +27,12 @@ exports.handler = (event, context, callback) => {
       body: global.state || ''
     })
   } else if (event.httpMethod === 'GET') {
-    if (event.queryStringParameters.password !== password) {
-      return callback(null, {
-        statusCode: 403,
-        body: 'Access denied'
-      })
-    }
+    // if (event.queryStringParameters.password !== password) {
+    //   return callback(null, {
+    //     statusCode: 403,
+    //     body: 'Access denied'
+    //   })
+    // }
 
     callback(null, {
       statusCode: 200,
