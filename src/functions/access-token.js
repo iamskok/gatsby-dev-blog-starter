ce7eaa7f35434b4f0d029c3ecc3a78fc0058d1e9
Vladimir Skok <skok@vova.io>
2019-07-21 12:26:17 -0700

    feat(build): move auth-redirect inside access-token netlify function

--- src/functions/access-token.js
+++ src/functions/access-token.js
@@ -1,5 +1,49 @@
 const axios = require('axios')
-const authRedirect = require('./templates/auth-redirect')
+const authRedirect = (query) => `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <script>
+      if (localStorage !== undefined) {
+        localStorage.setItem('github-token', '${query[`access_token`]}')
+      } else {
+        function setCookie(name, value, options = {}) {
+
+          options = {
+            path: '/',
+            // add other defaults here if necessary
+            ...options
+          };
+
+          if (options.expires && options.expires.toUTCString) {
+            options.expires = options.expires.toUTCString()
+          }
+
+          let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value)
+
+          for (let optionKey in options) {
+            updatedCookie += '; ' + optionKey
+            let optionValue = options[optionKey]
+            if (optionValue !== true) {
+              updatedCookie += '=' + optionValue
+            }
+          }
+
+          document.cookie = updatedCoo
+        }
+        // Set a cookie for 1 month
+        setCookie('github-token', '${query[`access_token`]}', {
+          'max-age': 2678400
+        });
+      }
+      location.href = '/blog/github-comments';
+    </script>
+  </body>
+  </html>`
 
 let host = ``
 let clientId = ``
