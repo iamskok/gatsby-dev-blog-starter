3f0d4f50225d8aa326780f74f20475b4546bf836
Vladimir <vladimir.skok@bvaccel.com>
2019-07-27 13:43:33 -0700

    add test netlify function

---
+++ src/functions/test.js
@@ -0,0 +1,12 @@
+const context = process.env.CONTEXT;
+const host = process.env.GATSBY_HOST_URL
+const clientId = process.env.GATSBY_GITHUB_CLIENT_ID
+const clientSecret = process.env.GATSBY_GITHUB_CLIENT_SECRET
+const password = process.env.GATSBY_FUNCTION_PASSWORD
+
+exports.handler = (event, context, callback) => {
+  callback(null, {
+    statusCode: 200,
+    body: `TEST:: context: ${context}, host: ${host}, clientId: ${clientId}, clientSecret: ${clientSecret}, password: ${password},`
+  })
+}
