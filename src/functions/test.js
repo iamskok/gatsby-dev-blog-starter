0b4c8c1f30980aa62d6ce9a3f2aa3c4a13bfbdb7
Vladimir Skok <skok@vova.io>
2019-07-27 14:23:35 -0700

    edit netlify test function

--- src/functions/test.js
+++ src/functions/test.js
@@ -1,4 +1,6 @@
 const context = process.env.GATSBY_CUSTOM_CONTEXT
+const context1 = process.env.GATSBY_CUSTOM_CONTEXT1
+const context2 = process.env.GATSBY_XXX
 const host = process.env.GATSBY_HOST_URL
 const clientId = process.env.GATSBY_GITHUB_CLIENT_ID
 const clientSecret = process.env.GATSBY_GITHUB_CLIENT_SECRET
@@ -7,6 +9,6 @@ const password = process.env.GATSBY_FUNCTION_PASSWORD
 exports.handler = (event, context, callback) => {
   callback(null, {
     statusCode: 200,
-    body: `TEST:: context: ${JSON.stringify(context)}, host: ${host}, clientId: ${clientId}, clientSecret: ${clientSecret}, password: ${password},`
+    body: `TEST:: context: ${JSON.stringify(context)}, context1: ${JSON.stringify(context1)}, context2: ${JSON.stringify(context2)}, host: ${host}, clientId: ${clientId}, clientSecret: ${clientSecret}, password: ${password},`
   })
 }
